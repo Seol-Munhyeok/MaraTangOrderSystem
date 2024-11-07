1a2b13bc0df3015c541c71d06db1347ff1709097
Seol-Munhyeok <abc1234568@naver.com>
2024-11-07 20:29:02 +0900

    feat(OrderService) : Add OrderService

---
+++ src/main/java/com/example/MaraTangOrderSystem/service/OrderService.java
@@ -0,0 +1,20 @@
+package com.example.MaraTangOrderSystem.service;
+
+import com.example.MaraTangOrderSystem.model.Order;
+import com.example.MaraTangOrderSystem.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+    private final OrderRepository orderRepository;
+
+    @Autowired
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public Order saveOrder(Order order) {
+        return orderRepository.save(order);
+    }
+}

81d5de48dae0c4b74dc9b065305fce8b4d311d9d
Seol-Munhyeok <abc1234568@naver.com>
2024-11-08 22:17:18 +0900

    fix(addOrder.js): js 문법 오류 수정

--- src/main/resources/static/js/addOrder.js
+++ src/main/resources/static/js/addOrder.js
@@ -1,13 +1,15 @@
 function addIngredient(ingredientName) {
+    console.log("Button clicked with ingredient:", ingredientName);
     const quantityInput = document.querySelector(`input[data-name="${ingredientName}"]`);
     const quantity = quantityInput ? quantityInput.value : 1;
 
     fetch(`/order/add/${ingredientName}?quantity=${quantity}`, {
         method: 'POST'
     })
-        .then(response => response.json())
+        .then(response => response)
         .then(data => {
-            alert(`${data.name} 추가 완료 (가격: ${data.price}원), 수량: ${quantity}`);
+            console.log("Response received:", data);
+            alert(`${data.ingredientName} 추가 완료 (가격: ${data.ingredientPrice}원), 수량: ${quantity}`);
         })
         .catch(error => {
             console.error('Error:', error);

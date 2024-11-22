8d9a3eff0ae556f7ddffb1dbf797e9f513454183
Seol-Munhyeok <abc1234568@naver.com>
2024-11-22 20:57:37 +0900

    refactor: OrderRequestDto, OrderResponseDto 간단히 수정
    인자를 간단하게 orderId만 넘기도록 수정함
    
    #3

--- src/main/java/com/example/MaraTangOrderSystem/dto/OrderResponseDto.java
+++ src/main/java/com/example/MaraTangOrderSystem/dto/OrderResponseDto.java
@@ -2,4 +2,4 @@ package com.example.MaraTangOrderSystem.dto;
 
 import java.util.List;
 
-public record OrderResponseDto(Long orderId, String userName, List<OrderDetailDto> orderDetails, Integer totalPrice) {}
+public record OrderResponseDto(Long orderId, Long userId, Integer totalPrice) {}
